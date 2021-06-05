5f498bb0be316dee01868d33a72228880cb96689
Will Sutton <wsutton17@gmail.com>
2021-06-04 20:25:31 -0400

    stage copy.lib complete

---
+++ stage_utils/copy.lib/mkstage.sh
@@ -0,0 +1,41 @@
+
+# "C:\Program Files\Git\usr\bin\bash.exe" mkstage.sh
+
+# update the remote url to new directory location
+base=$(pwd)
+
+cd remote
+cd main
+git init
+git checkout -b dummy
+cd ..
+cd ..
+
+cd teacher
+git init
+git remote add origin $base/remote/main
+git add book1.ipynb
+git commit -m "nb added"
+
+cp ../../../nbx ./ -r
+
+git add nbx
+git commit -m "nb + nbx added"
+
+git push origin master
+
+sleep 1
+
+# this, otherwise student can't clone properly
+cd ..
+cd remote
+cd main
+git checkout master
+git checkout -b dummy
+cd ..
+cd ..
+
+git clone $base/remote/main student
+cd student
+# git config pull.rebase true
+cd ..
